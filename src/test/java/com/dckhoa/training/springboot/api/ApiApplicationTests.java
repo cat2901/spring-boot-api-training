e28e798537b62e0d549e2b3d3498edde0bfd3d1a
dckhoa203 <khoadochi1998@gmail.com>
2021-05-23 03:32:52 +0700

    Get hello api

---
+++ src/test/java/com/dckhoa/training/springboot/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dckhoa.training.springboot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
